64a28a3db795a995efe54785cd368b23abf726c5
Mikhail Gorbadey <gormisha05@yandex.ru>
2022-10-02 19:44:42 +0500

    navbar,route config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,8 @@
-import { Suspense } from 'react';
-import { Link } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
 import './styles/index.scss'
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
-
+import { AppRouter } from 'app/providers/router';
+import { Navbar } from 'widgets/Navbar';
 
 export const App = () => {
 
@@ -14,15 +10,9 @@ export const App = () => {
 
   return (
     <div className={classNames('app', {}, [theme])}>
+      <Navbar />
+      <AppRouter />
       <button onClick={toggleTheme}>toggle theme</button>
-      <Link to={'/'}>Main Page</Link>
-      <Link to={'/about'}>About</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={'/'} element={<MainPage />} />
-          <Route path={'/about'} element={<AboutPage />} />
-        </Routes>
-      </Suspense>
     </div>
   );
 };
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import { classNames } from "shared/lib/classNames/classNames";
+import AppLink, { AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import cls from './Navbar.module.scss'
+
+interface NavbarProps {
+  className?: string
+}
+
+export const Navbar = ({ className }: NavbarProps) => {
+  return (
+    <div className={classNames(cls.Navbar, {}, [className])}>
+      <div className={cls.links}>
+        <AppLink to={'/'} className={cls.mainLink} theme={AppLinkTheme.SECONDARY}>
+          Main Page
+        </AppLink>
+        <AppLink to={'/about'} theme={AppLinkTheme.SECONDARY}>
+          About
+        </AppLink>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
