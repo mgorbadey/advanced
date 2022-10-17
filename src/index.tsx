53b35801f0f966702f7eebecab0ecf3856247e4f
Mikhail Gorbadey <gormisha05@yandex.ru>
2022-10-17 17:41:11 +0500

    move global css import to index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from 'app/providers/ThemeProvider';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import { App } from './app/App';
 import './shared/config/i18n/i18n';
+import 'app/styles/index.scss';
 
 render(
     <BrowserRouter>
