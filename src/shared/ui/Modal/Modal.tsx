cf4330646e00877dfc2eeb452a0c7b246a02502d
Mikhail Gorbadey <gormisha05@yandex.ru>
2022-10-20 17:05:50 +0500

    fix issue with modal theme

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -60,7 +60,6 @@ export const Modal = (props: ModalProps) => {
     const mods:Record<string, boolean> = {
         [cls.opened]: isOpen,
         [cls.isClosing]: isClosing,
-        [cls[theme]]: true,
     };
 
     return (
