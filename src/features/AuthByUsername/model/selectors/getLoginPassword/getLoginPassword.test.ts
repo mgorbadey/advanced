e33c1630f8dd1e0912741b86dd965a8a846a87de
Mikhail Gorbadey <gormisha05@yandex.ru>
2022-11-14 16:14:46 +0500

    add tests for AuthByUsername feature

---
+++ src/features/AuthByUsername/model/selectors/getLoginPassword/getLoginPassword.test.ts
@@ -0,0 +1,18 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getLoginPassword } from './getLoginPassword';
+
+describe('getLoginPassword.test', () => {
+    test('should return value', () => {
+        const state:DeepPartial<StateSchema> = {
+            loginForm: {
+                password: '1234',
+            },
+        };
+        expect(getLoginPassword(state as StateSchema)).toEqual('1234');
+    });
+    test('should work with empty state', () => {
+        const state:DeepPartial<StateSchema> = {};
+        expect(getLoginPassword(state as StateSchema)).toEqual('');
+    });
+});
