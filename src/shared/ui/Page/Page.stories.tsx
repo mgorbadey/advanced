9303b79a03a33113c2bf0afd29470c0f6d0f3fb6
Mikhail Gorbadey <gormisha05@yandex.ru>
2023-01-14 13:56:19 +0300

    fix linting

--- src/shared/ui/Page/Page.stories.tsx
+++ src/shared/ui/Page/Page.stories.tsx
@@ -3,17 +3,16 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Page } from './Page';
 
 export default {
-  title: 'shared/Page',
-  component: Page,
-argTypes: {
-  backgroundColor: { control: 'color' },
-},
+    title: 'shared/Page',
+    component: Page,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
 } as ComponentMeta < typeof Page>;
 
-const Template: ComponentStory<typeof Page> = (args) => < Page {...args } />;
+const Template: ComponentStory<typeof Page> = (args) => <Page {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
 
 };
-
